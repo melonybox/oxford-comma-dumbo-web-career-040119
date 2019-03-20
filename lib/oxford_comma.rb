c9340db3e71e53c481f426f607a49b8cdc988fbc
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 04:55:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array[0]
+  elsif array.size == 2
+    "#{array[0]} and #{array[1]}"
+  elsif array.size == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
+  else
+    newArray = []
+    i = 0
+    maxI = array.size - 1
+    until i == maxI
+      newArray.push(array[i])
+      i += 1
+    end
+    "#{newArray.join(", ")}, and #{array[-1]}"
+  
+  end
 end

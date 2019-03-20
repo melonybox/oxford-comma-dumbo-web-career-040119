8b031cbd2682f68fbeb58e24bbde99c21dc56b72
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 04:42:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
     array[0]
   elsif array.size == 2
     "#{array[0]} and #{array[1]}"
-  
+  elsif array.size == 3
+    "#{array[0]}, #{array[1]}, and #{array[2]}"
   
   end
 end
